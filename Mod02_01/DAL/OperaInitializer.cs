9b15160ed4f77351b3b441decc14c4e689584109
auxo86 <auxo86@gmail.com>
2017-05-09 20:04:37 +0800

    2002

---
+++ Mod02_01/DAL/OperaInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod02_01.DAL
+{
+    class OperaInitializer
+    {
+    }
+}
