6e2949c4c99a413c142d61d275ba9690909694f7
auxo86 <auxo86@gmail.com>
2017-05-16 20:02:24 +0800

    製作Log函式，並且限定只能在這個namespace用

--- Mod02_01/LogActionFilter.cs
+++ Mod02_01/LogActionFilter.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Diagnostics;
+using System.Web.Routing;
 
 
 namespace Mod02_01
@@ -14,22 +15,35 @@ namespace Mod02_01
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             base.OnActionExecuting(filterContext);
-            Debug.WriteLine("This Event Fired: OnActionExecuting");
+            //Debug.WriteLine("This Event Fired: OnActionExecuting");
+            Log("OnActionExecuting", filterContext.RouteData);
         }
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
             base.OnActionExecuted(filterContext);
-            Debug.WriteLine("This Event Fired: OnActionExecuted");
+            //Debug.WriteLine("This Event Fired: OnActionExecuted");
+            Log("OnActionExecuted", filterContext.RouteData);
         }
         public override void OnResultExecuting(ResultExecutingContext filterContext)
         {
             base.OnResultExecuting(filterContext);
-            Debug.WriteLine("This Event Fired: OnResultExecuting");
+            //Debug.WriteLine("This Event Fired: OnResultExecuting");
+            Log("OnResultExecuting", filterContext.RouteData);
         }
         public override void OnResultExecuted(ResultExecutedContext filterContext)
         {
             base.OnResultExecuted(filterContext);
-            Debug.WriteLine("This Event Fired: OnResultExecuted");
+            //Debug.WriteLine("This Event Fired: OnResultExecuted");
+            Log("OnResultExecuted", filterContext.RouteData);
+        }
+        
+        //製作Log函式，並且限定只能在這個namespace用
+        private void Log(string methodName, RouteData routeData)
+        {
+            var controllerName = routeData.Values["controller"];
+            var actionName = routeData.Values["action"];
+            var message = string.Format("{0} controler: {1}, action: {2}", methodName, controllerName, actionName);
+            Debug.WriteLine(message);
         }
     }
 
